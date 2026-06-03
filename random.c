3319347d3f91b4d010b53b064639e04e1dfa4dd3
Pranay <pranay.miyan@proton.me>
2026-06-03 23:11:53 +0000

    random.c stuff

--- random.c
+++ random.c
@@ -1,17 +1,25 @@
 /* random.c */
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int our_random_function() {
+
+int our_random_function(int max) {
     int x;
     
-    x = rand(10);
+    x = rand() % max++; // 
     return x;
 }
 
 int main() {
     int random;
 
-    random = our_random_function();
+    srand(getpid()); // getpid = processes count
+    random = our_random_function(5);
+    printf("%d\n", random);
+
+    random = our_random_function(10);
     printf("%d\n", random);
     
     return 0;
